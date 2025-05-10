f493dfbc816501bd1b6ab1422506ad9a7ae95b74
axeonyx <51489501+axeonyx@users.noreply.github.com>
2025-05-10 12:46:18 +0545

    Fixed Errors

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -433,8 +433,8 @@ const Index = () => {
     <div className="fade-in-element" ref={addToObserver} style={{ transitionDelay: '200ms' }}>
       <MetricsCard
         title="Click Rate"
-        value="8%"
-        description="Industry average: 5%"
+        value="4%"
+        description="Industry average: 2%"
         icon={
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M22 12H18L15 21L9 3L6 12H2" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
