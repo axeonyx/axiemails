ada6227f004cf5c20d7d55ffe9d4fee766fcc9c1
axeonyx <51489501+axeonyx@users.noreply.github.com>
2025-05-18 21:07:48 +0545

    New Changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -79,17 +79,16 @@ const Index = () => {
         </a>
       </div>
 
-      {/* Right: Hero Image (hidden on mobile) */}
       <div 
-        className="fade-in-element hidden md:flex lg:w-1/2 justify-center lg:justify-end" 
-        ref={addToObserver}
-      >
-        <img
-          src="/HeroImage.png"
-          alt="Illustration representing strategic email marketing for revenue growth"
-          className="w-full h-auto max-w-[600px] object-contain"
-        />
-      </div>
+  className="flex justify-center lg:justify-end w-full lg:w-1/2" 
+  ref={addToObserver}
+>
+  <img
+    src="/HeroImage.png"
+    alt="Illustration representing strategic email marketing for revenue growth"
+    className="w-full h-auto max-w-[600px] object-contain"
+  />
+</div>
     </div>
 
     {/* Social Proof (Inside Hero) */}
@@ -405,8 +404,8 @@ const Index = () => {
     <div className="fade-in-element" ref={addToObserver} style={{ transitionDelay: '100ms' }}>
       <MetricsCard
         title="Open Rate"
-        value="25%"
-        description="Industry average: 18%"
+        value="60%"
+        description="Industry average: 25%"
         icon={
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M3 9L12 2L21 9V20C21 20.5304 20.7893 21.0391 20.4142 21.4142C20.0391 21.7893 19.5304 22 19 22H5C4.46957 22 3.96086 21.7893 3.58579 21.4142C3.21071 21.0391 3 20.5304 3 20V9Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
